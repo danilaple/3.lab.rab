b89ba3d1d47d0e97f60ddf60017f6139c7b23a61
danilaple <99321805+danilaple@users.noreply.github.com>
2022-06-08 18:29:13 +0300

    Update индив.2.py

--- индив.2.py
+++ индив.2.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == '__main__' :
-
-a = int(input())
-if (a % 2 == 0):
-    print(a, "чётное")
-else:
-    print(a, "нечётное")
-b = int(input())
-if (b % 2 == 0):
-    print(b, "чётное")
-else:
-    print(b, "нечётное")
-c = int(input())
-if (c % 2 == 0):
-    print(c, "чётное")
-else:
-    print(c, "нечётное")
+    a = int(input())
+    if (a % 2 == 0):
+        print(a, "чётное")
+    else:
+        print(a, "нечётное")
+    b = int(input())
+    if (b % 2 == 0):
+        print(b, "чётное")
+    else:
+        print(b, "нечётное")
+    c = int(input())
+    if (c % 2 == 0):
+        print(c, "чётное")
+    else:
+        print(c, "нечётное")
