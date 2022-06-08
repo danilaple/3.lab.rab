2e691457359e1e83fb277297da936f2e07bbfa0e
danilaple <99321805+danilaple@users.noreply.github.com>
2022-06-08 18:29:41 +0300

    Update индив.3.py

--- индив.3.py
+++ индив.3.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == '__main__' :
-
-print("Таблица сложения: ")
-for i in range(1,10):
-    for j in range(1,10):
-        print(i,"+",j,"=",i+j)
+    print("Таблица сложения: ")
+    for i in range(1,10):
+        for j in range(1,10):
+            print(i,"+",j,"=",i+j)
